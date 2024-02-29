6b92267c6c5d9ac75b67f052faff81dc2dadcac0
CLARKSSQUARED <csubing2@gmail.com>
2024-02-29 15:47:32 +0800

    Adds hash_salt in the settings.php file.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -13,3 +13,5 @@ if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 }
 
 $settings['config_sync_directory'] = '../config/default';
+
+$settings['hash_salt'] = 'QrAm3ubePArmKr8H8pHhaVFahFRNGm2FzX-TDf2QES3Pi4bgGaAOOqsHQFy_k6JDl1ANG6XELg';
